9b54e4a774410ad0fdc64d17819e8efb2f3d5109
SamuelAndreass <164890124+SamuelAndreass@users.noreply.github.com>
2024-06-20 20:02:23 +0700

    Delete tugas directory

--- tugas/data/structJSON.go
+++
@@ -1,6 +0,0 @@
-package data
-
-type Person struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
--- tugas/tools/handleError.go
+++
@@ -1,9 +0,0 @@
-package tools
-
-import "fmt"
-
-func ErrorHandler(err error) {
-	if err != nil {
-		fmt.Println(err)
-	}
-}
